eec3b574325e55ec569e67d29de024b3615ca7d0
Raihan <raihandharma22@gmail.com>
2025-12-24 20:42:32 +0800

    editt

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,8 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
 
-mlflow.set_experiment("Adult Income CI")
-
 df = pd.read_csv("adult_preprocessed.csv")
 
 X = df.drop("income", axis=1)
@@ -18,11 +16,10 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 mlflow.autolog()
 
-with mlflow.start_run():
-    model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
+y_pred = model.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
 
-    print("Accuracy:", acc)
+print("Accuracy:", acc)
