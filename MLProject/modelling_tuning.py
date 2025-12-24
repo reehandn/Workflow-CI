0fd6312cec7afca8a2b1dcbdf6c833b5d4129220
Raihan <raihandharma22@gmail.com>
2025-12-24 21:11:53 +0800

    push

---
+++ MLProject/modelling_tuning.py
@@ -0,0 +1,100 @@
+import dagshub
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    f1_score,
+    confusion_matrix,
+    classification_report
+)
+
+# ===== DAGSHUB INIT (WAJIB) =====
+dagshub.init(
+    repo_owner="reehandn",
+    repo_name="adult-mlflow",
+    mlflow=True
+)
+
+# MLflow experiment (DAGSHUB)
+mlflow.set_experiment("Adult Income Advanced Tuning")
+
+# Load dataset
+df = pd.read_csv("adult_preprocessed.csv")
+
+X = df.drop("income", axis=1)
+y = df["income"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Hyperparameter tuning
+param_grid = [
+    {"C": 0.1, "solver": "lbfgs"},
+    {"C": 1.0, "solver": "lbfgs"},
+    {"C": 10.0, "solver": "lbfgs"},
+]
+
+# Training loop
+for params in param_grid:
+    with mlflow.start_run():
+        # -------- Log params --------
+        mlflow.log_param("C", params["C"])
+        mlflow.log_param("solver", params["solver"])
+        mlflow.log_param("penalty", "l2")
+        mlflow.log_param("max_iter", 1000)
+
+        # -------- Train model --------
+        model = LogisticRegression(
+            C=params["C"],
+            solver=params["solver"],
+            penalty="l2",
+            max_iter=1000
+        )
+        model.fit(X_train, y_train)
+
+        # -------- Prediction --------
+        y_pred = model.predict(X_test)
+
+        # -------- Metrics (autolog-like) --------
+        acc = accuracy_score(y_test, y_pred)
+        prec = precision_score(y_test, y_pred)
+        rec = recall_score(y_test, y_pred)
+        f1 = f1_score(y_test, y_pred)
+
+        mlflow.log_metric("accuracy", acc)
+        mlflow.log_metric("precision", prec)
+        mlflow.log_metric("recall", rec)
+        mlflow.log_metric("f1_score", f1)
+
+        # Confusion Matrix (PNG)
+        cm = confusion_matrix(y_test, y_pred)
+        plt.figure()
+        plt.imshow(cm)
+        plt.title("Confusion Matrix")
+        plt.colorbar()
+        plt.xlabel("Predicted")
+        plt.ylabel("Actual")
+        plt.tight_layout()
+        plt.savefig("confusion_matrix.png")
+        plt.close()
+
+        mlflow.log_artifact("confusion_matrix.png")
+
+    
+        report = classification_report(y_test, y_pred)
+        with open("classification_report.txt", "w") as f:
+            f.write(report)
+
+        mlflow.log_artifact("classification_report.txt")
+
+        mlflow.sklearn.log_model(model, "model")
+
+        print(f"Finished run: params={params}, accuracy={acc:.4f}")
