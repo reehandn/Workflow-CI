423e63f377b26e43fe1a39f7f94687834fb22c88
Raihan <raihandharma22@gmail.com>
2025-12-24 18:16:04 +0800

    revisi modelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
 
+# Load data
 df = pd.read_csv("adult_preprocessed.csv")
 
 X = df.drop("income", axis=1)
@@ -14,12 +15,20 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-mlflow.autolog()
+with mlflow.start_run():
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
 
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
 
-y_pred = model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
+    # Log metric
+    mlflow.log_metric("accuracy", acc)
 
-print("Accuracy:", acc)
+    # Log model (INI YANG PENTING)
+    mlflow.sklearn.log_model(
+        model,
+        name="model"
+    )
+
+    print("Accuracy:", acc)
