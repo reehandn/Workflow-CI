307d8265e056213950c67867149fbabb0d12c9aa
Raihan <raihandharma22@gmail.com>
2025-12-24 20:11:45 +0800

    rombak kode

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -4,20 +4,35 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
+import warnings
 
-df = pd.read_csv("adult_preprocessed.csv")
+warnings.filterwarnings("ignore")
 
-X = df.drop("income", axis=1)
-y = df["income"]
+if __name__ == "__main__":
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+    df = pd.read_csv("adult_preprocessed.csv")
 
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
+    X = df.drop("income", axis=1)
+    y = df["income"]
 
-y_pred = model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
-print("Accuracy:", acc)
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    print("Accuracy:", acc)
+
+    mlflow.log_metric("accuracy", acc)
+
+    input_example = X_train.iloc[:5]
+
+    mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="model",
+        input_example=input_example
+    )
